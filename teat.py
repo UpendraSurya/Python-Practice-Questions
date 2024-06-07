6773d9f1303023a3a7d648d12cb9b2f7a0a9742e
Jonnalagadda Upendra surya narayana gupta <121785180+UpendraSurya@users.noreply.github.com>
2024-06-07 12:48:21 +0530

    Delete teat.py

--- teat.py
+++
@@ -1,7 +0,0 @@
-def filtering(namesss,max_length):
-    return list(filter(lambda name: len(name)<=max_length,namesss))
-
-namesss_k=["John","Smith","yuva","alex","narayana"]
-short_ee=filtering(namesss_k,4)
-print(short_ee)
-#%%
